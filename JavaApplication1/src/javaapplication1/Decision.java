abc5f5cf5f3566d62d43a151a97b1eb7ec20bf7d
cherlanda <cherlanda@gmail.com>
2016-01-21 05:23:39 +0700

    Java
    Penggunaan R serve:
    1. Install R 3.1.3
    2. install package Rserve di R
    3. Masukkan library Rserve dan REngine di IDE
    4. masukkan code :
    library(Rserve)
    Rserve()
    di dalam program R
    
    5. Rserve telah dapat digunakan di Java

---
+++ JavaApplication1/src/javaapplication1/Decision.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author sayut_000
+ */
+public class Decision {
+    private String cashPointId;
+    private String lokasi;
+    private double sislok;
+    private double tarikan;
+    private double forecastAccuracy;
+
+    public String getCashPointId() {
+        return cashPointId;
+    }
+
+    public void setCashPointId(String cashPointId) {
+        this.cashPointId = cashPointId;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public double getSislok() {
+        return sislok;
+    }
+
+    public void setSislok(double sislok) {
+        this.sislok = sislok;
+    }
+
+    public double getTarikan() {
+        return tarikan;
+    }
+
+    public void setTarikan(double tarikan) {
+        this.tarikan = tarikan;
+    }
+
+    public double getForecastAccuracy() {
+        return forecastAccuracy;
+    }
+
+    public void setForecastAccuracy(double forecastAccuracy) {
+        this.forecastAccuracy = forecastAccuracy;
+    }
+}
